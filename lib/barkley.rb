249ff620dfb1958fd2faedf30cdcf5a569522db4
pjaspers <piet@pjaspers.com>
2024-03-19 09:16:04 +0100

    Add info on Fun Run

--- lib/barkley.rb
+++ lib/barkley.rb
@@ -6,6 +6,8 @@ module Barkley
     def elapsed = Time.now - start
     def final_cut_off = start + duration_to_sec("60:00:00")
     def full_run_cut_off = start + duration_to_sec("36:00:00")
+    # Fun run is three loops, once you start for a 4th it's no longer a fun run
+    # Need to finish before 40h
     def fun_run_cut_off = start + duration_to_sec("40:00:00")
 
     private def duration_to_sec(duration)
