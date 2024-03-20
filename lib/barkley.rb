8eded3a7f45e525b694ecfae343045e7ea7a9b73
pjaspers <piet@pjaspers.com>
2024-03-20 10:05:34 +0100

    Add starttime

--- lib/barkley.rb
+++ lib/barkley.rb
@@ -2,7 +2,8 @@ module Barkley
   class << self
     attr_accessor :runners
 
-    def start = Time.now
+    def conch_blown = Time.new(2024, 03, 20, 4, 17, 0, "-04:00")
+    def start = conch_blown + 1*60*60
     def elapsed = Time.now - start
     def final_cut_off = start + duration_to_sec("60:00:00")
     def full_run_cut_off = start + duration_to_sec("36:00:00")
