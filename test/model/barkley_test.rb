78a45a251b84fd998e012d55ed6a3a3be04ba3ed
pjaspers <piet@pjaspers.com>
2024-03-20 23:25:45 +0100

    Remove unused test

--- test/model/barkley_test.rb
+++ test/model/barkley_test.rb
@@ -4,7 +4,6 @@ class BarkleyTest < Test
   it "cutoffs" do
     refute_nil Barkley.fun_run_cut_off
     refute_nil Barkley.full_run_cut_off
-    refute_nil Barkley.final_cut_off
     refute_nil Barkley.elapsed
     refute_nil Barkley.start
   end
