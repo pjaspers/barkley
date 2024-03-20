7ca04fcab91176c6d5b36c66933c57c12537ca8e
pjaspers <piet@pjaspers.com>
2024-03-20 23:24:09 +0100

    Adding cutoffs

--- lib/barkley.rb
+++ lib/barkley.rb
@@ -5,7 +5,7 @@ module Barkley
     def conch_blown = Time.new(2024, 03, 20, 4, 17, 0, "-04:00")
     def start = conch_blown + 1*60*60
     def elapsed = Time.now - start
-    def final_cut_off = start + duration_to_sec("60:00:00")
+    def end_time = start + duration_to_sec("60:00:00")
     def full_run_cut_off = start + duration_to_sec("36:00:00")
     # Fun run is three loops, once you start for a 4th it's no longer a fun run
     # Need to finish before 40h
