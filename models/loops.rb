de374f44a959669be8db914b710c7a30c7a41dfb
pjaspers <piet@pjaspers.com>
2024-03-21 19:00:57 +0100

    And Jasmin is on Loop 4

--- models/loops.rb
+++ models/loops.rb
@@ -78,7 +78,7 @@ data = {
     Loop.new(1, start: "00:00:00", stop: "08:30:59"),
     Loop.new(2, start: nil, stop: "19:27:52"),
     Loop.new(3, start: "19:45:51", stop: ["32:15:53", m[112134878192699850], t[1770866384513249421]]),
-    Loop.new(4, start: nil, stop: nil),
+    Loop.new(4, start: ["32:27:50", m[112134925987883714],t[1770869408589574173]], stop: nil),
     Loop.new(5, start: nil, stop: nil),
   ],
   # Guessing because we only know that these came in a few moments after the first three
